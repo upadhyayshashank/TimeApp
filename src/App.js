5fd574d7cd6d870bf4f669d3aecb49c28285c4e4
Upadhyayshashank <shashank.shashank.upadhyay7@gmail.com>
2020-08-22 13:24:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let currDate = new Date();
+    currDate = currDate.getHours();
+
+    let greeting = "";
+
+    const cssStyle = {};
+
+    if (currDate >= 1 && currDate < 12) {
+        greeting = "Good Morning";
+        cssStyle.color = "#02c39a";
+    } else if (currDate >= 12 && currDate < 16) {
+        greeting = "Good Afternoon";
+        cssStyle.color = "#ffba08";
+    } else if (currDate >= 16 && currDate < 20) {
+        greeting = "Good Evening";
+        cssStyle.color = "#e76f51";
+    } else {
+        greeting = "Good Night";
+        cssStyle.color = "#6c757d";
+    }
+
+    return (
+        <React.Fragment>
+            <div>
+                <h1>
+                    Hello Shashank, <span style={cssStyle}> {greeting} </span>
+                </h1>
+            </div>
+        </React.Fragment>
+    )
 }
 
 export default App;
